4125da9b53e741c8d2bcc2a3e17207d2716cfb05
Auke <32795119+AukeB@users.noreply.github.com>
2019-03-11 16:21:19 +0100

    Add files via upload
    Divided it more into functions

--- NN_ass_1/NN_ass_01_04.py
+++ NN_ass_1/NN_ass_01_04.py
@@ -2,84 +2,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import sys
+from scipy.spatial import distance
 
-DATA_PATH = "/home/auke/MEGA/Study/Courses/2018-2019/Neural_Networks/Assignments/Assignment_01/data/"
+# Paths and filenames.
+DATA_PATH = "/vol/home/s1594443/Desktop/NN/Assignment_01/data/"
 train_in_path = "train_in.csv"
 train_out_path = "train_out.csv"
 test_in_path = "test_in.csv"
 test_out_path = "test_out.csv"
 
-train_in = np.genfromtxt ( DATA_PATH + train_in_path, dtype = 'float', delimiter = ',' )
-train_out = np.genfromtxt ( DATA_PATH + train_out_path, dtype = 'float', delimiter = ',' )
-test_in = np.genfromtxt ( DATA_PATH + test_in_path, dtype = 'float', delimiter = ',' )
-test_out = np.genfromtxt ( DATA_PATH + test_out_path, dtype = 'float', delimiter = ',' )
+# Importing data.
+train_in = np.genfromtxt(DATA_PATH+train_in_path,dtype='float',delimiter=',')
+train_out = np.genfromtxt(DATA_PATH+train_out_path,dtype='float',delimiter=',')
+test_in = np.genfromtxt(DATA_PATH+test_in_path,dtype='float',delimiter=',')
+test_out = np.genfromtxt(DATA_PATH+test_out_path,dtype='float',delimiter=',')
 
-dim_1_train = len ( train_in ) # 1707. Number of digits in training data.
-dim_1_test = len ( test_in ) # 1000. Number of digits in testing data.
-dim_2 = len ( train_in[0] ) # 256. 256 dimensional space.
-dim_3 = 10 # Number of possible digits.
+# Functions.
+def train_perceptron(data_in,labels,weights): # Changes weights.
+	iteration = 0 # Counts the amount of iterations done before 100% correct classification is reached.
+	output_indices = np.zeros((len(data_in),1)) # Initializing array.
+	weighted_sum = np.zeros((len(data_in),len(weights[0]))) # Initializing array.
 
-def train_perceptron ( data_in, data_out ):	
-	bias = np.zeros ( ( len ( data_in ), 1 ) ) # Initialise bias array.
-	data_in = np.append ( data_in, bias, axis = 1 ) # Merge data_in and weights.
-	weights = np.random.rand ( 257, 10 ) # Initialise weights array.randomly.
+	while np.array_equal(output_indices,labels) == False: # While there are still incorrectly classified digits ...
+		false_classified_indices = [] # Array that will contain all indices of wrong
+		iteration += 1 # Counts the number of iterations needed to reach 100%.
 
-	weighted_sum = np.dot ( data_in, weights ) # Compute weighted sum.
-	output_indices = np.argmax ( weighted_sum, axis = 1 ) # Select maximum value.
+		for i in range(len(data_in)):
+			if output_indices[i] != labels[i]:
+				false_classified_indices.append(i)
 
-	epoch = 0
+		percentage_correct = (len(data_in)-len(false_classified_indices))*100/float(len(data_in))
 
-	while np.array_equal ( output_indices, data_out ) == False:
-		wrongly_classified_indices = []
+		plt.plot(iteration,percentage_correct,'.')
+		sys.stdout.write("{:03.1f}% ".format(percentage_correct))
+		sys.stdout.write ( " {0}\r".format(iteration)); sys.stdout.flush ()
 
-		for i in range ( len ( data_in ) ):
-			if output_indices[i] != data_out[i]:
-				wrongly_classified_indices.append ( i )
+		k = false_classified_indices[random.randint(0,len(false_classified_indices)-1)] # Choose a random element that is incorrectly classified.
 
-		if epoch % 10 == 0: plt.plot ( epoch, ( len ( data_in ) - len ( wrongly_classified_indices ) ) * 100 / float ( len ( data_in ) ), '.' )
+		for j in range(len(weights[0])): # The algorithm that changes the weights.
+			if weighted_sum[k][j] > weighted_sum[k][int(labels[k])]:
+				weights[:,j] -= data_in[k] # Changing of the weights.
+			if j == int ( labels[k] ):
+				weights[:,j] += data_in[k] # Changing of the weights.
 
-		sys.stdout.write ( "{:03.1f}% ".format ( ( len ( data_in ) - len ( wrongly_classified_indices ) ) * 100 / float ( len ( data_in ) ) ) )
-		sys.stdout.write ( " {0}\r".format ( epoch ) )
-		sys.stdout.flush ()
+		weighted_sum = np.dot(data_in,weights)
+		output_indices = np.argmax(weighted_sum,axis=1)
 
-		k = wrongly_classified_indices[random.randint ( 0, len ( wrongly_classified_indices ) - 1 ) ]
-
-		for j in range ( dim_3 ):
-			if weighted_sum[k][j] > weighted_sum[k][int(data_out[k])]:
-				weights[:,j] -= data_in[k]
-			if j == int ( data_out[k] ):
-				weights[:,j] += data_in[k]
-
-		weighted_sum = np.dot ( data_in, weights )
-		output_indices = np.argmax ( weighted_sum, axis = 1 )
-
-		epoch += 1
-
-	sys.stdout.write ( "Iterations: {0}\n".format ( epoch ) )
-
-	plt.show () # Toggle
+	sys.stdout.write ( "Iterations: {0}\n".format ( iteration ) )
+	plt.xlabel("Number of iterations.")
+	plt.ylabel("Percentage correctly classified digits.")
+	plt.show ()
 
 	return weights
 
-def test_perceptron ( data_in, data_out, weights ):
-	bias = np.zeros ( ( len ( data_in ), 1 ) ) # Initialise bias array.
-	data_in = np.append ( data_in, bias, axis = 1 ) # Merge data_in and weights.
+def test_perceptron ( data_in, labels, weights ): # Test new weights values for a certain dataset.
+	weighted_sum = np.dot ( data_in, weights ) # Definition
+	output_indices = np.argmax ( weighted_sum, axis = 1 ) # Get the index with highest value.
 
-	weighted_sum = np.dot ( data_in, weights )
-	output_indices = np.argmax ( weighted_sum, axis = 1 )
+	correct_counter = 0 # Amount of correctly classified digits.
 
-	correct_counter = 0
-
-	for i in range ( len ( data_in ) ):
-		if output_indices[i] == data_out[i]:
+	for i in range(len(data_in)):
+		if output_indices[i] == labels[i]:
 			correct_counter += 1
 
-	sys.stdout.write ( "Accuracy:   {:03.1f}%\n".format ( correct_counter * 100 / len ( data_out ) ) )
+	sys.stdout.write ( "Accuracy:   {:03.1f}%\n".format(correct_counter*100/len(labels)))
 
-for i in range ( 1 ):
-	sys.stdout.write ( "Run {0}\n".format ( i + 1 ) )
-	weights = train_perceptron ( train_in, train_out )
-	test_perceptron ( train_in, train_out, weights )
-	test_perceptron ( test_in, test_out, weights )
-	sys.stdout.write ( "\n" )
+def perceptron(data_in,labels,weights,train):
+	bias = np.ones((len(data_in),1)) # Create bias array filled with ones.
+	data_in = np.append(data_in,bias,axis=1) # Merge data_in and bias.
+	if train == True: return train_perceptron(data_in,labels,weights) # Training program.
+	if train == False: test_perceptron(data_in,labels,weights) # Testing program.
 
+def main(n): # n = Number of times you want to run the program.
+	for i in range(n):
+		sys.stdout.write ( "Run {0}\n".format ( i + 1 ) )
+		weights = np.random.rand(257,10) # Initialise weights array.randomly.
+		weights = perceptron(train_in,train_out,weights,train=True) # Train the weights.
+		perceptron(train_in,train_out,weights,train=False) # Test weights on train_data (should be 100%).
+		perceptron(test_in,test_out,weights,train=False) # Test weights on test_data.
+		sys.stdout.write ( "\n" )
+
+main(2)
